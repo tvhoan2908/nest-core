01bbb11d6dfdba1fa30b7e7897837d5e33461cac
Hoan Trinh <hoantv@vuihoc.vn>
2022-11-01 16:55:35 +0700

    chore: initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    tsconfigRootDir : __dirname, 
+    sourceType: 'module',
+  },
+  plugins: [
+    '@typescript-eslint/eslint-plugin',
+    "unused-imports"
+  ],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+  },
+  ignorePatterns: ['.eslintrc.js', "commitlint.config.js"],
+  rules: {
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    semi: "error",
+    quotes: ["error", "double"],
+    indent: ["error", 2, { SwitchCase: 1 }],
+    "no-unused-vars": ["error", { "vars": "all" }],
+    "no-unused-vars": "off",
+    "unused-imports/no-unused-imports-ts": 2,
+    'prettier/prettier': ['error', { singleQuote: false }]
+  },
+};
